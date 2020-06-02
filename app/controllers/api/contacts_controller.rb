af8f9cf5d126e14cb4294a6ffe4740d4d8f5ab8a
carlhauck <carlhauck@gmail.com>
2020-06-02 13:48:46 -0500

    makes all routing actions RESTful

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,42 @@
+class Api::ContactsController < ApplicationController
+
+  def index
+    @contacts = Contact.all
+    render "index.json.jb"
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+  
+  def create
+    @contact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.update(
+      first_name: params[:first_name] || @contact.first_name,
+      last_name: params[:last_name] || @contact.last_name,
+      email: params[:email] || @contact.email,
+      phone_number: params[:phone_number] || @contact.phone_number
+    )
+    @contact.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+    render json: {message: "The contact has been deleted."}
+  end
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# contact = Contact.new(first_name: "Chumley", last_name: "Thomas", email: "chuminthewater@aol.com", phone_number: "355-434-8295")
+# contact.save
+
+# contact = Contact.new(first_name: "Donkey", last_name: "Kong", email: "barrelofmonkeys@gmail.com", phone_number: "972-682-4974")
+# contact.save
