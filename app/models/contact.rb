54373ff7866c31137eb10279572719d9b552808c
carlhauck <carlhauck@gmail.com>
2020-06-03 11:58:08 -0500

    makes show and index views render a partial

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,15 @@
 class Contact < ApplicationRecord
+
+  def friendly_updated_at
+    updated_at.strftime("%-d %B %Y %-l:%M%P")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def japanese_phone_number
+    "+81 #{phone_number}"
+  end
+
 end
